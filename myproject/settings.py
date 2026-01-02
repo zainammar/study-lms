eb7df7f9215715379418fcc1727a3d6974bfd878
zainammar <zainammar74@gmail.com>
2026-01-02 12:16:49 +0500

    Add logo on Top and bottom add fotter

--- myproject/settings.py
+++ myproject/settings.py
@@ -83,8 +83,12 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
